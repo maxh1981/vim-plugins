5506804adece9ce709103eb2ef5abda80008294a
TC Huang <maxh1981@gmail.com>
2015-04-26 02:18:25 +0000

    Fixed operation not aborted on failure

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
+set -e
+
 if ! [ -d bin/setup.d ]; then
 	printf "can't locate bin/setup.d/\n" >&2
 	exit 1
 fi
 
 for SCRIPT in `ls bin/setup.d/*.sh | sort`; do
-	printf "executing $SCRIPT..." &&
-	$SCRIPT &&
-	printf "done\n" &&
-	continue
-
-	print "failed\n" &&
-	exit 1
+	printf "executing $SCRIPT..."
+	"$SCRIPT"
+	printf "done\n"
 done
