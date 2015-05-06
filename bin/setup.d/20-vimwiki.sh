8f02ea31e701998ee1f50b7e91932b5d42254b4a
Max Huang <maxh1981@gmail.com>
2015-05-06 08:55:06 +0800

    Mapped C-l, C-h to shift between diaries

--- bin/setup.d/20-vimwiki.sh
+++ bin/setup.d/20-vimwiki.sh
@@ -8,3 +8,5 @@ git clone https://github.com/vim-scripts/vimwiki.git
 echo 'set nocompatible' >> "$HOME/.vimrc"
 echo 'filetype plugin on' >> "$HOME/.vimrc"
 echo 'syntax on' >> "$HOME/.vimrc"
+echo 'nmap <C-l> <Plug>VimwikiDiaryNextDay' >> "$HOME/.vimrc"
+echo 'nmap <C-h> <Plug>VimwikiDiaryPrevDay' >> "$HOME/.vimrc"
