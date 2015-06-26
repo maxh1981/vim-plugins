4fbd56dd7a49d7c291d09de1571bb1cfc419c06e
Max Huang <maxh1981@gmail.com>
2015-06-26 15:51:51 +0800

    Updated configuration

--- bin/setup.d/01-vimrc.sh
+++ bin/setup.d/01-vimrc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo 'set number' >> "$HOME/.vimrc"
-echo 'syntax sync fromstart' >> "$HOME/.vimrc"
+echo 'noremap <F12> <Esc>:syntax sync fromstart<CR>' >> "$HOME/.vimrc"
+echo 'inoremap <F12> <C-o>:syntax sync fromstart<CR>' >> "$HOME/.vimrc"
