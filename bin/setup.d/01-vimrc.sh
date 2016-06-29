b3d0160f03d19871efc179477f17960001fedcc3
Max Huang <maxh1981@gmail.com>
2016-06-29 15:07:44 +0800

    Set default background to dark

--- bin/setup.d/01-vimrc.sh
+++ bin/setup.d/01-vimrc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo 'set number' >> "$HOME/.vimrc"
+echo 'set bg=dark' >> "$HOME/.vimrc"
 echo 'noremap <F12> <Esc>:syntax sync fromstart<CR>' >> "$HOME/.vimrc"
 echo 'inoremap <F12> <C-o>:syntax sync fromstart<CR>' >> "$HOME/.vimrc"
