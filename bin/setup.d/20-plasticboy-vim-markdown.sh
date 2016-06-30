7489eb3a6304537fd1f747d741909944d5360323
Max Huang <maxh1981@gmail.com>
2016-06-30 17:28:55 +0800

    Update vim-markdown plugin requirement

--- bin/setup.d/20-plasticboy-vim-markdown.sh
+++ bin/setup.d/20-plasticboy-vim-markdown.sh
@@ -5,4 +5,7 @@ set -e
 cd "$HOME/.vim/bundle"
 git clone https://github.com/plasticboy/vim-markdown.git
 
+# required to use `:TableFormat`
+git clone https://github.com/godlygeek/tabular.git
+
 echo "let g:vim_markdown_folding_disabled=1" >> ~/.vimrc
